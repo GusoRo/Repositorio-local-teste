dbaad88b4dd3351d59785d9bbd518f74adc09c0f
GusoRo <gustavo.aprendiz09@gmail.com>
2026-08-03 22:38:13 -0300

    Melhora do entendimento do código atráves de comentarios

--- Controle_preços.java
+++ Controle_preços.java
@@ -3,12 +3,15 @@ import java.util.ArrayList;
 
 void main() {
 
+    //Cria lista de preços
     ArrayList<Double> preco = new ArrayList<Double>();
 
+    //Utiliza do for para adicionar os valores dos produtos um por um
     for (int i = 0; i < 5; i++) {
         preco.add(Double.parseDouble(readln("Informe o valor do " + (i + 1) + "° item: ")));
     }
 
+    //Escaneia a lista e printa o menor e o maior preço na lista
     IO.println("=========================================\n"+
             "O menor preço é "+(Collections.min(preco)+"\n")+
             "=========================================");
